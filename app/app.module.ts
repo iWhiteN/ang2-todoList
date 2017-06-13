a6a87805e88ffd3587c0c1ac29a10a4fb7c51731
Roman (work) <whiten.ncs@gmail.com>
2017-06-13 17:27:59 +0300

    refactor

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,17 @@
-import {NgModule} from "@angular/core";
-import {BrowserModule} from "@angular/platform-browser";
-import {FormsModule} from "@angular/forms";
-import {HttpModule} from '@angular/http';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from '@angular/http';
 
-import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
-import {InMemoryDataService} from './shared/data.service';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './shared/data.service';
 
-import {AppComponent} from './app.component';
-import {TodosComponent} from './todos/todos.component';
-import {TodoFormComponent} from './todos/todo-form/todo-form.component';
-import {TodoListComponent} from './todos/todo-list/todo-list.component';
-import {TodoItemComponent} from './todos/todo-item/todo-item.component';
-import {TodoService} from './shared/todo.service';
+import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import { TodoFormComponent } from './todos/todo-form/todo-form.component';
+import { TodoListComponent } from './todos/todo-list/todo-list.component';
+import { TodoItemComponent } from './todos/todo-item/todo-item.component';
+import { TodoService } from './shared/todo.service';
 
 @NgModule({
     imports: [
--- app/shared/todo.service.ts
+++ app/shared/todo.service.ts
@@ -1,13 +1,12 @@
-import {Injectable} from '@angular/core';
-import {Http, Headers, RequestOptions} from '@angular/http';
-import {Observable} from "rxjs/Observable";
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { Observable } from "rxjs/Observable";
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 
-
-import {Todo} from './todo';
+import { Todo } from './todo';
 
 @Injectable()
 export class TodoService {
